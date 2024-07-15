7c2027813a3fe3c2612c945dac33531dca84be8b
Nikita Vereshchagin <nikita.vereshchagin@ansiblehealth.com>
2024-07-15 03:02:12 +0300

    Delete some comments

--- src/main/java/com/zaurtregulov/spring/data/entity/MaintenanceSchedule.java
+++ src/main/java/com/zaurtregulov/spring/data/entity/MaintenanceSchedule.java
@@ -5,62 +5,6 @@ import com.zaurtregulov.spring.data.entity.enums.MaintenanceScheduleType;
 import jakarta.persistence.*;
 import java.util.Date;
 
-//@Entity
-//@Table(name = "maintenance_schedule")
-//public class MaintenanceSchedule {
-//
-//    private Room room;
-//    private Date maintenanceDate;
-//    private MaintenanceScheduleType maintenanceType;
-//    private MaintenanceScheduleStatus status;
-//
-//    public MaintenanceSchedule(Room room, Date maintenanceDate, MaintenanceScheduleType maintenanceType, MaintenanceScheduleStatus status) {
-//        this.room = room;
-//        this.maintenanceDate = maintenanceDate;
-//        this.maintenanceType = maintenanceType;
-//        this.status = status;
-//    }
-//
-//    protected MaintenanceSchedule() {}
-//
-//    @OneToOne
-//    @JoinColumn(name = "room_id", unique = true)
-//    public Room getRoom() {
-//        return room;
-//    }
-//
-//    public void setRoom(Room room) {
-//        this.room = room;
-//    }
-//
-//    @Column(name = "maintenance_date", nullable = false)
-//    public Date getMaintenanceDate() {
-//        return maintenanceDate;
-//    }
-//
-//    public void setMaintenanceDate(Date maintenanceDate) {
-//        this.maintenanceDate = maintenanceDate;
-//    }
-//
-//    @Column(name = "maintenance_type", nullable = false)
-//    public MaintenanceScheduleType getMaintenanceType() {
-//        return maintenanceType;
-//    }
-//
-//    public void setMaintenanceType(MaintenanceScheduleType maintenanceType) {
-//        this.maintenanceType = maintenanceType;
-//    }
-//
-//    @Column(name = "status", nullable = false)
-//    public MaintenanceScheduleStatus getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(MaintenanceScheduleStatus status) {
-//        this.status = status;
-//    }
-//}
-
 @Entity
 @Table(name = "maintenance_schedule")
 public class MaintenanceSchedule {
